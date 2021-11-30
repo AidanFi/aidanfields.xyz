21b6b42dfba5b8f4d6f27ec3fdccb3b723f0434b
Aidan Fields <aidan.fields@sckans.edu>
2021-11-30 12:46:58 -0600

    edits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Hero from './Hero';
+import Footer from './Footer';
 import './App.css';
-
+import email from './email.svg';
+import resume from './resume.svg';
+import github from './github.svg';
+import linkedin from './linkedin.svg';
 
 function App() {
   return (
@@ -14,6 +18,22 @@ function App() {
       <div class="student">
         Computer Science Student
       </div>
+      <hr class="line"/>
+      <div class="links">
+        <a href="mailto:aidanfields12@gmail.com">
+          <img src={email} />
+        </a>
+        <a href="INSERT RESUME HERE">
+          <img src={resume} />
+        </a>
+        <a href="https://github.com/AidanFi">
+          <img src={github} />
+        </a>
+        <a href="https://www.linkedin.com/in/aidan-fields-a648541a7/">
+          <img src={linkedin} />
+        </a>
+        </ div>
+      <Footer />
     </div>
   );
 }
