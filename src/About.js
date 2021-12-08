8a4f7c2047b19734ee6af615ca6923d9a069e2cd
Aidan Fields <aidan.fields@sckans.edu>
2021-12-07 23:52:07 -0600

    edits

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from './af.jpg';
 import { Link } from 'react-router-dom';
 import './App.css';
+import Portrait from './portrait.jpg';
 
 function About() {
     return (
@@ -24,6 +25,15 @@ function About() {
             About Me
           </div>
           <hr class="line2"/>
+          <div class="about-me-content">
+            I am currently a computer science student-athlete at Southwestern College in Winfield, Kansas.
+            I plan to work in software development, with other interests being articifial inteligence/machine learning, ui/ux design, 
+            computer graphics, 3D modeling, and game development. <br/> <br/>
+            I know the C++ and Python programming languages, as well as 
+            HTML, CSS, and JavaScript. I also have experience working in Windows and Linux, and I am able to code using functional 
+            and object-oriented programming.
+          </div>
+          <div class="circle"/>
           </div>
 
         
--- src/Contact.js
+++ src/Contact.js
@@ -6,25 +6,68 @@ import './App.css';
 function Contact() {
     return (
         <div className="App">
-        <header className="App-header">
-          <div>
-            <img src={Logo} class="logo"/>
-          </div>
-          <div class="topnav">
-            <Link class="home" to="/">Home</Link>
-            <hr class="vertical" />
-            <Link class="about" to="/about">About</Link>
-            <hr class="vertical" />
-            <Link class="contact" to="/contact">Contact</Link>
-            <hr class="vertical" />
-            <Link class="projects" to="/projects">Projects</Link>
-          </div>
+          <header className="App-header">
+            <div>
+              <img src={Logo} class="logo"/>
+            </div>
+            <div class="topnav">
+              <Link class="home" to="/">Home</Link>
+              <hr class="vertical" />
+              <Link class="about" to="/about">About</Link>
+              <hr class="vertical" />
+              <Link class="contact" to="/contact">Contact</Link>
+              <hr class="vertical" />
+              <Link class="projects" to="/projects">Projects</Link>
+           </div>
           </header>
           <div class="title">
             Contact Me
           </div>
           <hr class="line2"/>
+
+          <div class="container">
+            <div class="message-me">Message me</div>
+            <form action="action_page.php">
+
+              <label for="name">Name</label>
+              <input type="text" id="name" name="name" placeholder="Your name.."></input>
+
+              <label for="email">Email</label>
+              <input type="text" id="email" name="email" placeholder="Your email.."></input>
+
+              <label for="subject">Subject</label>
+              <input type="text" id="subject" name="subject" placeholder="Subject.."></input>
+
+              <label for="message">Message</label>
+              <textarea type="text" id="message" name="message" placeholder="Type message here.."></textarea>
+
+              <input type="submit" value="Submit"></input>
+
+            </form>
           </div>
+          <div class="contact-info">
+            Contact Information
+            <div class="info">
+              Name
+              <div class="info-sub">
+                Aidan Fields
+              </div>
+              Location
+              <div class="info-sub">
+                Winfield, Kansas, United States
+              </div>
+              Cell phone
+              <div class="info-sub">
+                (+1)620-241-7421
+              </div>
+              Email
+              <div class="info-sub">
+                aidanfields12@gmail.com
+              </div>
+            </div>
+          </div>
+
+        </div>
     );
 }
 
