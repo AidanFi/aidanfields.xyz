88a1337e58ccaf93b523512bdb01263ce4013e6b
Aidan Fields <aidan.fields@sckans.edu>
2021-12-09 10:06:57 -0600

    edits

--- src/Contact.js
+++ src/Contact.js
@@ -24,48 +24,47 @@ function Contact() {
             Contact Me
           </div>
           <hr class="line2"/>
+            <div class="container">
+              <div class="message-me">Message me</div>
+              <form action="action_page.php">
 
-          <div class="container">
-            <div class="message-me">Message me</div>
-            <form action="action_page.php">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Your name.."></input>
 
-              <label for="name">Name</label>
-              <input type="text" id="name" name="name" placeholder="Your name.."></input>
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" placeholder="Your email.."></input>
 
-              <label for="email">Email</label>
-              <input type="text" id="email" name="email" placeholder="Your email.."></input>
+                <label for="subject">Subject</label>
+                <input type="text" id="subject" name="subject" placeholder="Subject.."></input>
 
-              <label for="subject">Subject</label>
-              <input type="text" id="subject" name="subject" placeholder="Subject.."></input>
+                <label for="message">Message</label>
+                <textarea type="text" id="message" name="message" placeholder="Type message here.."></textarea>
 
-              <label for="message">Message</label>
-              <textarea type="text" id="message" name="message" placeholder="Type message here.."></textarea>
+                <input type="submit" value="Submit"></input>
 
-              <input type="submit" value="Submit"></input>
-
-            </form>
-          </div>
-          <div class="contact-info">
-            Contact Information
-            <div class="info">
-              Name
-              <div class="info-sub">
-                Aidan Fields
-              </div>
-              Location
-              <div class="info-sub">
-                Winfield, Kansas, United States
-              </div>
-              Cell phone
-              <div class="info-sub">
-                (+1)620-241-7421
-              </div>
-              Email
-              <div class="info-sub">
-                aidanfields12@gmail.com
+              </form>
+            </div>
+            <div class="contact-info">
+              Contact Information
+              <div class="info">
+                Name
+                <div class="info-sub">
+                  Aidan Fields
+                </div>
+                Location
+                <div class="info-sub">
+                  Winfield, Kansas, United States
+                </div>
+                Cell phone
+                <div class="info-sub">
+                  (+1)620-241-7421
+                </div>
+                Email
+                <div class="info-sub">
+                  aidanfields12@gmail.com
+                </div>
               </div>
             </div>
-          </div>
 
         </div>
     );
